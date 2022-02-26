6b846d4b97477eed4208be99654a7b2c1cf8e336
jorddo <jordanbsneed@gmail.com>
2022-02-26 17:20:01 -0600

    manager/engineer tests

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,11 +1,13 @@
 const Engineer = require('../lib/Engineer');
 
-test('creates engineer object', () => {
-  const engineer = new Engineer('Alec');
+describe('Engineer', () => {
+  test('creates engineer object', () => {
+    const engineer = new Engineer('Alec', 'alec@fakemail.com', 'ibealec');
 
-  // need name, employee id, email address, github username
-  expect(engineer.name).toBe('Alec');
-  expect(engineer.id).toEqual(expect.any(Number));
-  expect(engineer.email).toBe('alec@fakemail.com');
-  expect(engineer.github).toBe('ibealec');
+    // need name, employee id, email address, github username
+    expect(engineer.name).toBe('Alec');
+    expect(engineer.id).toEqual(expect.any(Number));
+    expect(engineer.email).toBe('alec@fakemail.com');
+    expect(engineer.github).toBe('ibealec');
+  });
 });
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,11 +1,13 @@
 const Manager = require('../lib/Manager');
 
-test('creates a manager object', () => {
-  const manager = new Manager('Jared');
+describe('Manager', () => {
+  test('creates a manager object', () => {
+    const manager = new Manager('Jared', 'jared@fakemail.com');
 
-  // need name, employee id, email address, office number
-  expect(manager.name).toBe('Jared');
-  expect(manager.id).toEqual(expect.any(Number));
-  expect(manager.email).toBe('jared@fakemail.com');
-  expect(manager.office).toEqual(expect.any(Number));
+    // need name, employee id, email address, office number
+    expect(manager.name).toBe('Jared');
+    expect(manager.id).toEqual(expect.any(Number));
+    expect(manager.email).toBe('jared@fakemail.com');
+    expect(manager.office).toEqual(expect.any(Number));
+  });
 });
