b734baf6ac1a3013300f16b464e4a4911164c188
jorddo <jordanbsneed@gmail.com>
2022-02-26 17:02:34 -0600

    Co-authored-by: Jimmie Jackson <jimmiejackson414@gmail.com>

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -2,11 +2,11 @@ const Intern = require('../lib/Intern');
 
 describe('Intern', () => {
   test('creates the intern object', () => {
-    const intern = new Intern('Jordan', 'jordan@daddy.daddy', 'harvard');
+    const intern = new Intern('Jordan', 'jordan@fakemail.com', 'harvard');
 
     expect(intern.name).toBe('Jordan');
     expect(intern.id).toEqual(expect.any(Number));
-    expect(intern.email).toBe('jordan@daddy.daddy');
+    expect(intern.email).toBe('jordan@fakemail.com');
     expect(intern.college).toBe('harvard');
   });
 });
